be848ef51a0606617847767d36b85b785fcb4a36
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-07-28 11:58:11 +0100

    change script pathing

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.static("views"));
 app.use(express.static("scripts"));
+app.use(express.static("..public/scripts"));
 
 //Function to retrieve data for posts.json
 function getData() {
