0661dda88cf4638e9cee28f11496e118e45a31bb
johnpascual <ashleypascual@hotmail.es>
2022-07-28 10:15:40 +0100

    everything

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ const fs = require("fs");
 const ejs = require("ejs");
 const _ = require("lodash");
 const path = require("path");
-let posts = ("./public/posts.json");
+const supervillains = require('supervillains');
+let posts = ("../public/posts.json");
 // const cors = require("cors");
 const app = express();
 
@@ -13,38 +14,39 @@ app.use(express.json());
 // app.use(cors());
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(express.static("../client/public"));
 
+app.use(express.static("public"));
+app.use(express.static("views"));
+app.use(express.static("../views"));
+app.use(express.static("../public"));
 
 
 function getData() {
-  let data = fs.readFileSync("./public/posts.json");
+  let data = fs.readFileSync("../public/posts.json");
   data = JSON.parse(data);
   return data;
 }
 
 //Function to store data
 function storeData(req) {
-  data = getData("./public/posts.json");
+  data = getData("../public/posts.json");
   data.posts.push(req);
   let myJSON = JSON.stringify(data, null, 2);
-  fs.writeFileSync("./public/posts.json", myJSON);
+  fs.writeFileSync("../public/posts.json", myJSON);
 }
 
 
 app.get("/", (req, res) => {
   currentData = getData();
-  res.render("index", { currentData: currentData });
+  res.render(__dirname + "/../views/index", { currentData: currentData });
 });
 
 app.get("/post", (req, res) => {
-  res.render("post");
+  res.render(__dirname + "/../views/post");
 });
 
 
 app.post("/post", (req, res) => {
-  data = req.body;
   currentData = getData();
   // Function to iterate through the data to find the highest ID
   let highestId = 0;
@@ -57,36 +59,21 @@ app.post("/post", (req, res) => {
   let id = highestId + 1;
   let title = req.body.title;
   let text = req.body.text;
-  let comments = req.body.comments;
+  let gif = req.body.gif
+  let date = getPostDate()
+  let supervillain = supervillains.random()
+  //let comments = req.body.comments;
   storeData({
     id: id,
     title: title,
     text: text,
     comments: [],
+    gif: gif,
+    time: date,
+    supervillain: supervillain
   });
   res.redirect("/");
 });
-// Add comments
-app.post("/comments/:id", (req, res) => {
-  let id = req.params.id;
-  let newComment = req.body.comments;
-  let newData = getData();
-  newData.posts.forEach((post) => {
-    if (post.id == id) {
-      post.comments.push(newComment);
-    }
-  });
-  let myJson = JSON.stringify(newData, null, 2);
-  fs.writeFileSync("../public/post.json", myJson, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("../public/post.json");
-    }
-  });
-  res.redirect("/");
-});
-
 
 // Deleting a post
 app.delete("/posts/:id", (req, res) => {
@@ -107,20 +94,117 @@ app.delete("/posts/:id", (req, res) => {
   res.send("Deletion Complete!");
 });
 
-
-app.get("/postPage/:postName", (req, res) => {
-  let postName = _.lowerCase(req.params.postName);
+app.get("/postPage/:id", (req, res) => {
+  let postId = Number(req.params.id)
   currentData = getData();
   for (let i = 0; i < currentData.posts.length; i++) {
-    if (_.lowerCase(currentData.posts[i].title) === postName) {
-      res.render("postPage", {
+    if (currentData.posts[i].id === postId) {
+      res.render(__dirname + "/../views/postPage", {
         title: currentData.posts[i].title,
         text: currentData.posts[i].text,
+        comments: currentData.posts[i].comments,
+        id: currentData.posts[i].id,
+        gif: currentData.posts[i].gif,
+        date: currentData.posts[i].time,
+        supervillain : currentData.posts[i].supervillain
       });
     }
   }
 });
 
+app.post("/postPage/:id", (req, res) => {
+  let postId = Number(req.params.id)
+  let newComment = req.body.comment
+  currentData = getData();
+
+  for (let i = 0; i < currentData.posts.length; i++) {
+    if (postId === currentData.posts[i].id) {
+      currentData.posts[i].comments.push(newComment)
+      let myJSON = JSON.stringify(currentData, null, 2);
+      fs.writeFileSync("../public/posts.json", myJSON);
+      res.render(__dirname + "/../views/postPage", {
+        title: currentData.posts[i].title, 
+        text: currentData.posts[i].text, 
+        comments: currentData.posts[i].comments, 
+        id: currentData.posts[i].id,
+        gif: currentData.posts[i].gif,
+        date: currentData.posts[i].time,
+        supervillain: currentData.posts[i].supervillain
+      })
+    }
+  }
+
+})
+
+
+// Add comments
+// app.post("/comments/:id", (req, res) => {
+//   let id = req.params.id;
+//   let newComment = req.body.comments;
+//   let newData = getData();
+//   newData.posts.forEach((post) => {
+//     if (post.id == id) {
+//       post.comments.push(newComment);
+//     }
+//   });
+//   let myJson = JSON.stringify(newData, null, 2);
+//   fs.writeFileSync("../public/post.json", myJson, (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("../public/post.json");
+//     }
+//   });
+//   res.redirect("/");
+// });
+
+
+// this is a function to get the time and date a post was made
+
+function getPostDate() {
+
+  let date = new Date()
+
+  hour = date.getHours() 
+  hour = hour < 10 ? `0${hour}` : hour
+  minute = date.getMinutes()
+  minute = minute < 10 ? `0${minute}` : minute
+  day = date.getDate()
+  month = date.getMonth()
+  year = date.getFullYear()
+
+  //minute = minute < 10 ? `0${minute}` : minute
+
+  if (month === 1) {
+      month = "Jan"
+  } else if (month === 2) {
+      month = "Feb"
+  } else if (month === 3) {
+      month = "Mar"
+  } else if (month === 4) {
+      month = "Apr"
+  } else if (month === 5) {
+      month = "May"
+  } else if (month === 6) {
+      month = "Jul"
+  } else if (month === 7) {
+      month = "Aug"
+  } else if (month === 8) {
+      month = "Sep"
+  } else if (month === 9) {
+      month = "Oct"
+  } else if (month === 10) {
+      month = "Nov"
+  } else if (month === 11) {
+      month = "Dec"
+  } else if (month === 0) {
+      
+  }
+
+  let postDate = `${hour}:${minute} · ${month} ${day}, ${year}`
+
+  return postDate
+}
 
 module.exports = app;
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
 // Start the server
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Started listening on port ${port}`);
